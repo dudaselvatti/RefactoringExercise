c16b4072500a10bb116f1183a3c12315803ad0a3
dudaselvatti <dudaselvatti@hotmail.com>
2025-10-17 00:10:37 -0300

    Correção simples de erro na classe de conjurados

--- updaters/Conjured.py
+++ updaters/Conjured.py
@@ -5,4 +5,4 @@ class ConjuredItem:
         item.sell_in = item.sell_in - 2
         if item.sell_in < 0:
             if item.quality > 0:
-                item.quality = item.quality - 2
+                item.quality = item.quality - 4
